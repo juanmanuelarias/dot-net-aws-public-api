4b44ab539c799205ab1346072b2fc8a995ee76ba
Juan Arias <juan.arias@xero.com>
2017-05-05 15:41:17 +1200

    Test GitHub hook

--- PublicAPI/Modules/ApiModule.cs
+++ PublicAPI/Modules/ApiModule.cs
@@ -9,6 +9,7 @@ namespace PublicAPI.Modules
             Get("/ping", args => new Response {StatusCode = HttpStatusCode.OK});
             Get("/healthcheck", args => new Response { StatusCode = HttpStatusCode.OK });
             Get("/test", args => "hello world!");
+            Get("/test2", args => "hello world 2!");
         }
     }
 }
